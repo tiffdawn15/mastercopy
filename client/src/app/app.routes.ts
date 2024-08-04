f22441e17f775731526f6af2517d38a09b663bcb
Tiffany Messer <tiffanymesser@macbook-pro.mynetworksettings.com>
2024-08-04 16:35:44 -0400

    Image page and information is loading.

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,43 @@
 import { Routes } from '@angular/router';
+import { PictureBoardComponent } from './picture-board/picture-board.component';
+import { ExploreComponent } from './explore/explore.component';
+import { ArtWorkComponent } from './art-work/art-work.component';
 
-export const routes: Routes = [];
+// export const routes: Routes = [
+//   {
+//     path: '',
+//     loadComponent: () =>
+//       import('./picture-board/picture-board.component').then(
+//         (mod) => mod.PictureBoardComponent
+//       ),
+//   },
+//   {
+//     path: 'explore',
+//     pathMatch: 'full',
+//     loadComponent: () =>
+//       import('./explore/explore.component').then((mod) => mod.ExploreComponent),
+//   },
+//   {
+//     path: `artwork/{$id}`,
+//     pathMatch: 'full',
+//     loadComponent: () =>
+//       import('./art-work/art-work.component').then((mod) => mod.ArtWorkComponent),
+//   },
+//   {
+//     path: '**',
+//     loadComponent: () =>
+//       import('./picture-board/picture-board.component').then(
+//         (mod) => mod.PictureBoardComponent
+//       ),
+//   }
+// ];
+
+export const routes: Routes = [
+  { path: '', component: PictureBoardComponent },
+  { path: 'explore', component: ExploreComponent },
+  {
+    path: `artwork/:id}`,
+    loadComponent: () =>
+      import('./art-work/art-work.component').then( (mod) => mod.ArtWorkComponent),
+  },
+];
