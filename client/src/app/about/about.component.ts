20744d8b2f469a3be86c6f2854d998d7f36637d3
Tiffany Messer <tiffanymesser@tiffanys-mbp.mynetworksettings.com>
2024-10-02 13:46:19 -0400

    Added some of the about elements

--- client/src/app/about/about.component.ts
+++ client/src/app/about/about.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [
+    HeaderComponent
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
 
+
+  onSearch(term: string) {
+    console.log(term);
+
+    // TODO: Implement this function
+    
+  }
 }
