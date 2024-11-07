b9f993e25c65d8b555a7ce923eee9b839b9f7cfa
Tiffany Messer <tiffanymesser@tiffanys-mbp.mynetworksettings.com>
2024-11-07 15:27:53 -0500

    Updated to angular material form

--- client/src/app/about/about.component.ts
+++ client/src/app/about/about.component.ts
@@ -1,67 +1,74 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { ParallaxDirective } from '../parallax.directive';
-import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { ContactService } from '../contact.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-about',
   standalone: true,
   imports: [
-    HeaderComponent, 
+    HeaderComponent,
+    MatInputModule,
+    MatFormFieldModule,
     ParallaxDirective,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrl: './about.component.css',
 })
 export class AboutComponent {
   contactForm = new FormGroup({
-    name: new FormControl("", Validators.min(3)),
-    email: new FormControl("", Validators.min(3)),
-    message: new FormControl("", Validators.min(3)),
+    name: new FormControl('', Validators.min(3)),
+    email: new FormControl('', Validators.min(3)),
+    message: new FormControl('', Validators.min(3)),
   });
 
   constructor(private contactService: ContactService) {}
-
 
   onSearch(term: string) {
     console.log(term);
 
     // TODO: Implement this function
-    
   }
 
   onSubmit() {
     console.log(this.contactForm.value);
     const from = {
       name:
-        this.contactForm?.get("name")?.value !== null
-          ? this.contactForm.get("name")?.value
-          : "",
-      email: this.contactForm.get("email")?.value,
+        this.contactForm?.get('name')?.value !== null
+          ? this.contactForm.get('name')?.value
+          : '',
+      email: this.contactForm.get('email')?.value,
     };
 
     const to = [
       {
-        name: "Tiff",
-        email: "tiffanymesser15@gmail.com",
+        name: 'Tiff',
+        email: 'tiffanymesser15@gmail.com',
       },
     ];
     // TODO: Implement Good object practices here
     const message = {
       sender: from,
       to: to,
-      htmlContent: this.contactForm.get("message")?.value,
-      subject: "Email from our Website",
+      htmlContent: this.contactForm.get('message')?.value,
+      subject: 'Email from our Website',
       replyTo: from,
     };
 
     console.log(message);
 
     this.contactService.sendEmail(message).subscribe(
-      (response) => console.log("Success!", response),
-      (error) => console.error("Error!", error)
+      (response) => console.log('Success!', response),
+      (error) => console.error('Error!', error)
     );
   }
 }
