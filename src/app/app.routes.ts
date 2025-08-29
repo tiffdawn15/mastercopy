b9cd262b7900179284bbe155ee4f95404fbf2fc7
Tiffany Messer <tiffanymesser15@gmail.com>
2025-08-29 13:46:03 -0400

    Replaced with CSR build. Images are now showing. Next step styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,21 @@
-import { Routes } from "@angular/router";
-import { PictureBoardComponent } from "./picture-board/picture-board.component";
-import { AboutComponent } from "./about/about.component";
-import { ClientTestComponent } from "./client-test/client-test.component";
-import { LoginButtonComponent } from "./login-button/login-button.component";
-import { ArtWorkComponent } from "./art-work/art-work.component";
+import { Routes } from '@angular/router';
+import { About } from './about/about';
+import { Board } from './board/board';
+import { User} from './user/user.js'
+import { AuthGuard } from '@auth0/auth0-angular';
 
 export const routes: Routes = [
-  { path: "", component: PictureBoardComponent,
-  },
-  { path: "about", component: AboutComponent },
-  { path: "client", component: ClientTestComponent },
-  { path: "login", component: LoginButtonComponent },
-  { path: "artwork", component: ArtWorkComponent },
-
-  // {
-  //   path: `artwork/:id`,
-  //   loadComponent: () =>
-  //     import("./art-work/art-work.component").then(
-  //       (mod) => mod.ArtWorkComponent
-  //     ),
-  // },
-  // { path: '**', redirectTo: '' },
+    { path: "", component: Board},
+    { path: "about", component: About },
+    { path: "user", component: User, 
+      canActivate: [AuthGuard],
+    },
+    {
+      path: `artwork/:id`,
+      loadComponent: () =>
+        import("./artwork/artwork.js").then(
+          (mod) => mod.Artwork
+        ),
+    },
+    { path: '**', redirectTo: '' },
 ];
